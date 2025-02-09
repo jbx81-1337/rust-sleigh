f5e7f50adab71a15cde380e42b0fd4c3e7ecdd99
jbx81-1337 <jbx81@protonmail.com>
2025-02-09 11:10:10 +0100

    fix: compile sleigh only if not present

--- build_sleigh.sh
+++ build_sleigh.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-git submodule init
-git submodule update
-cd deps/sleigh
-cmake -B build -S .
-cmake --build build --parallel 8
-cmake --install build --prefix ./../../vendor/
+if [ ! -f "./vendor/lib/libsla.a" ]; then
+    git submodule init
+    git submodule update
+    cd deps/sleigh
+    cmake -B build -S .
+    cmake --build build --parallel 8
+    cmake --install build --prefix ./../../vendor/
+fi
